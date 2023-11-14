ccad7d2a6466102c10d9afe303c03d96f5aea0de
seyyednavid hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-11-14 23:34:44 +0000

    Create Home page - Consider it as root path

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes } from 'react-router-dom';
-import { Container, Typography } from '@mui/material';
+import Home from "./pages/Home.js";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import { Container, Typography } from '@mui/material';
+
 
 function App() {
   return (
     <Router>
-      <Container>
-        <Typography variant="h6">test: its working </Typography>
+      {/* <Container> */}
+        {/* <Typography variant="h6">test: its working </Typography> */}
         <Routes>
-        {/* Your routes go here */}
+          <Route path="/" element={<Home />} />
         </Routes>
-      </Container>
+      {/* </Container> */}
     </Router>
   );
 }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <>
+      <h1>welcome to home page</h1>
+    </>
+  );
+};
+
+export default Home;
