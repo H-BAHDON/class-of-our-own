f5e4d68a6b3b5b12a2b6d9c36301957d2761a3ba
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-11-17 13:17:54 +0000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Container, Typography } from "@mui/material";
 import NavBar from "./components/NavBar"
 import TraineeDashboard from "./pages/TraineeDashboard";
 import Home from "./pages/Home.js";
+import Footer from "./components/Footer.js";
 
 function App() {
   return (
     <Router>
-      <Container>
-      <NavBar />
+      {/* <NavBar /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/trainee/" element={<TraineeDashboard />} />
         </Routes>
-     </Container>
+        <Footer />
     </Router>
   );
 }
