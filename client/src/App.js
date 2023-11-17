c2b03481e94ab9ae8e88af5c9d13b4ac6ab72fb8
seyyednavid hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-11-17 00:02:29 +0000

    Update Navbar button for GitHub Authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Container, Typography } from "@mui/material";
-import NavBar from "./components/NavBar"
+import NavBar from "./components/NavBar";
 import TraineeDashboard from "./pages/TraineeDashboard";
 import Home from "./pages/Home.js";
 
@@ -9,12 +9,12 @@ function App() {
   return (
     <Router>
       <Container>
-      <NavBar />
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/trainee/" element={<TraineeDashboard />} />
         </Routes>
-     </Container>
+      </Container>
     </Router>
   );
 }
