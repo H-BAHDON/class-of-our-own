b6a111912085cc2a1973b409e16926f5d7687754
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-11-20 15:18:22 +0000

    Created configAxios.js

---
+++ client/src/config/configAxios.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+const apiUrl = "http://localhost:3001";
+
+function configAxios(config) {
+    const instance = axios.create({
+        baseURL: apiUrl,
+        withCredentials: true,
+        ...config,
+    });
+
+    instance.interceptors.request.use(
+        (c) => {
+            const token = `Bearer ${getCookie('access_token')}`;
+            c.baseURL = apiUrl;
+            if (token) {
+                c.headers.authorization = token;
+            }
+            return c;
+        },
+        (error) => Promise.reject(error),
+    );
+
+
+    instance.interceptors.response.use(
+        function (response) {
+            if (response.data) {
+                if (response.status === 200 || response.status === 201) {
+                    return response;
+                }
+                return Promise.reject(response);
+            }
+            return Promise.reject(response);
+        },
+        function (error) {
+            return Promise.reject(error);
+        }
+    );
+
+    return instance;
+}
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+export default { configAxios, apiUrl };
