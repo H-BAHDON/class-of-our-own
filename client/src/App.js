640b1220f48176a741be8e27b30d9693572fbb4b
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-11-22 13:49:08 +0000

    Added post-Sign form for the trainee and blocked the volunteer

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Container, Typography } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { Container } from "@mui/material";
 import NavBar from "./components/NavBar";
 import TraineeDashboard from "./pages/TraineeDashboard";
 import Home from "./pages/Home.js";
 import Footer from "./components/Footer.js";
 import { AuthProvider } from './hooks/useAuth.js';
-
+import PostSignup from "./pages/Post-Signup Page/Post-signup.jsx";
 
 function App() {
+
+  // Function to determine whether to render NavBar based on the current route
+  const shouldRenderNavBar = () => {
+    const excludedRoutes = ["/PostSignup"]; // Add routes where NavBar should be excluded
+    return !excludedRoutes.includes(window.location.pathname);
+  };
+
   return (
     <Router>
       <AuthProvider>
-        <NavBar />
+        {shouldRenderNavBar() && <NavBar />}
         <Container>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/PostSignup" element={<PostSignup />} />
             <Route path="/trainee/" element={<TraineeDashboard />} />
           </Routes>
           <Footer />
