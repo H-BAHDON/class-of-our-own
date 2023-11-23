8d010799cacda4c0ab2a267a3408666df8dacb58
PaulinaWywrot <wywrot.paula@gmail.com>
2023-11-23 14:34:57 +0000

    updated user.js

--- server/models/user.js
+++ server/models/user.js
@@ -7,8 +7,8 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      this.belongsTo(models.Cohort, { foreignKey: "cohortId" });
+    static associate({ Cohort }) {
+      this.belongsTo(Cohort, { foreignKey: "cohortId" });
     }
   }
   User.init(
