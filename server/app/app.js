4613e7587357ff7b9380deb0e6cddefc25c7d1c5
seyyednavid hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-11-23 22:34:54 +0000

    Create endpoint to return current milestone

--- server/app/app.js
+++ server/app/app.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const passwordSetup = require("./middleware/passport");
-const { sequelize, User } = require("../models");
+const { sequelize, User, Cohort, Milestone } = require("../models");
 
 dotenv.config();
 
@@ -49,6 +49,47 @@ const userRoutes = require("./routes/userRoute");
 app.use("/auth", authRoutes);
 app.use("/signpost", signpostRoute);
 app.use("/user", userRoutes);
+
+app.get("/current-milestone", async (req, res) => {
+  try {
+    const email = req.user.email;
+
+    const user = await User.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.status(401).json({ error: "User is not authenticated" });
+    }
+
+    if (user.role !== "trainee") {
+      return res.status(422).json({ error: "User is not a trainee" });
+    }
+
+    const currentDate = new Date();
+
+    const currentMilestone = await Milestone.findOne({
+      where: {
+        cohortId: user.cohortId,
+        startDate: { $lte: currentDate },
+        endDate: { $gte: currentDate },
+      },
+    });
+
+    // Return milestone information
+    if (currentMilestone) {
+      return res.json({
+        milestoneName: currentMilestone.name,
+        startDate: currentMilestone.startDate,
+        endDate: currentMilestone.endDate,
+      });
+    } else {
+      return res.json({ message: "No current milestone found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 // Error handling middleware
 app.use((err, req, res, next) => {
