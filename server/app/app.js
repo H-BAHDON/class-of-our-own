654878a591fc7e14560221ce032ef8cf21283da1
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-11-24 15:19:54 +0000

    Updated app.js

--- server/app/app.js
+++ server/app/app.js
@@ -45,51 +45,13 @@ app.get("/", (req, res) => {
 const authRoutes = require("./routes/authRoutes");
 const signpostRoute = require("./routes/signpostRoute");
 const userRoutes = require("./routes/userRoute");
+const milestoneRoute = require("./routes/milestone");
 
 app.use("/auth", authRoutes);
 app.use("/signpost", signpostRoute);
 app.use("/user", userRoutes);
+app.use("/current-milestone", milestoneRoute);
 
-app.get("/current-milestone", async (req, res) => {
-  try {
-    const email = req.user.email;
-
-    const user = await User.findOne({
-      where: { email },
-    });
-
-    if (!user) {
-      return res.status(401).json({ error: "User is not authenticated" });
-    }
-
-    if (user.role !== "trainee") {
-      return res.status(422).json({ error: "User is not a trainee" });
-    }
-
-    const currentDate = new Date();
-
-    const currentMilestone = await Milestone.findOne({
-      where: {
-        cohortId: user.cohortId,
-        startDate: { $lte: currentDate },
-        endDate: { $gte: currentDate },
-      },
-    });
-
-    // Return milestone information
-    if (currentMilestone) {
-      return res.json({
-        milestoneName: currentMilestone.name,
-        startDate: currentMilestone.startDate,
-        endDate: currentMilestone.endDate,
-      });
-    } else {
-      return res.json({ message: "No current milestone found" });
-    }
-  } catch (error) {
-    return res.status(500).json({ error: "Internal Server Error" });
-  }
-});
 
 // Error handling middleware
 app.use((err, req, res, next) => {
