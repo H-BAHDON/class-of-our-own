8b3dc5ce170c8e03d4c546d324b108f55c5c217a
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-11-24 15:20:22 +0000

    Create milestone.js

---
+++ server/app/routes/milestone.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const { authenticateUser } = require("../middleware/authMiddleware");
+const { HandleCurrentMilestone } = require("../controllers/milestoneController")
+
+
+router.get("/", authenticateUser, HandleCurrentMilestone);
+
+router.get("/", authenticateUser, HandleGetAllMillestones);
+
+module.exports = router;
