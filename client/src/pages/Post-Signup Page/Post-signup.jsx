8451b961c0dbe4241e984848e2e06f7895335b0b
seyyednavid hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-11-28 18:26:56 +0000

    Change the button and footer content color - chenge the context on signup form

--- client/src/pages/Post-Signup Page/Post-signup.jsx
+++ client/src/pages/Post-Signup Page/Post-signup.jsx
@@ -8,6 +8,7 @@ import {
   Tab,
   Paper,
 } from "@mui/material";
+import NavBar from "../../components/NavBar";
 
 export default function PostSignup() {
   const [activeTab, setActiveTab] = React.useState(0);
@@ -34,7 +35,7 @@ export default function PostSignup() {
           codeWarsUsername,
           codilityUsername,
         }),
-        credentials: 'include',
+        credentials: "include",
       });
       if (!response.ok) {
         console.error("Error:", response.statusText);
@@ -42,112 +43,114 @@ export default function PostSignup() {
       }
       const responseData = await response.json();
       console.log(responseData);
-      window.location.href = 'http://localhost:3000/trainee';
-
+      window.location.href = "http://localhost:3000/trainee";
     } catch (error) {
       console.error("Error sending data to the backend:", error);
     }
   };
 
   return (
-    <Box sx={{ maxWidth: 400, margin: "auto", mt: 4 }}>
-      <Paper elevation={3} sx={{ padding: 2 }}>
-        <Tabs value={activeTab} onChange={handleChangeTab} centered>
-          <Tab label="Trainee" />
-          <Tab label="Volunteer" />
-        </Tabs>
+    <>
+      <NavBar />
+      <Box sx={{ maxWidth: 400, margin: "auto", mt: 4 }}>
+        <Paper elevation={3} sx={{ padding: 2 }}>
+          <Tabs value={activeTab} onChange={handleChangeTab} centered>
+            <Tab label="Trainee" />
+            <Tab label="Volunteer" />
+          </Tabs>
 
-        <Box sx={{ mt: 2 }}>
-          {activeTab === 0 && (
-            <form action="/" method="post" onSubmit={handleSubmitForTrainee}>
-              <Typography variant="h5" align="center" mb={2}>
-                Welcome to CYF Rookie, we need some data
-              </Typography>
-
-              <TextField
-                label="CodeWars UserName"
-                variant="outlined"
-                fullWidth
-                margin="normal"
-                required
-                onChange={(e) => setCodeWarsUsername(e.target.value)}
-              />
-
-              <TextField
-                label="Codility"
-                variant="outlined"
-                fullWidth
-                margin="normal"
-                required
-                onChange={(e) => setCodilityUsername(e.target.value)}
-              />
-
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                fullWidth
-              >
-                Get Started
-              </Button>
-            </form>
-          )}
-
-          {activeTab === 1 && (
-            <Box sx={{ position: "relative" }}>
-              <form action="/" method="post">
+          <Box sx={{ mt: 2 }}>
+            {activeTab === 0 && (
+              <form action="/" method="post" onSubmit={handleSubmitForTrainee}>
                 <Typography variant="h5" align="center" mb={2}>
-                  Welcome Volunteer!
+                  Welcome to CYF Rookie, we need some data
                 </Typography>
 
                 <TextField
-                  label="Full Name"
+                  label="Codewars UserName"
                   variant="outlined"
                   fullWidth
                   margin="normal"
                   required
+                  onChange={(e) => setCodeWarsUsername(e.target.value)}
                 />
 
                 <TextField
-                  label=""
+                  label="Codility (optional)"
                   variant="outlined"
                   fullWidth
                   margin="normal"
                   required
+                  onChange={(e) => setCodilityUsername(e.target.value)}
                 />
 
                 <Button
                   type="submit"
                   variant="contained"
-                  color="primary"
                   fullWidth
+                  style={{backgroundColor:"rgb(237,67,67)", color: "white"}}
                 >
-                  Log In
+                  Get Started
                 </Button>
               </form>
+            )}
 
-              <Box
-                sx={{
-                  position: "absolute",
-                  top: 0,
-                  left: 0,
-                  width: "100%",
-                  height: "100%",
-                  backgroundColor: "rgba(0, 0, 0, 0.5)",
-                  zIndex: 1,
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Typography variant="body2" color="textSecondary">
-                  Feature Coming Soon
-                </Typography>
+            {activeTab === 1 && (
+              <Box sx={{ position: "relative" }}>
+                <form action="/" method="post">
+                  <Typography variant="h5" align="center" mb={2}>
+                    Welcome Volunteer!
+                  </Typography>
+
+                  <TextField
+                    label="Full Name"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    required
+                  />
+
+                  <TextField
+                    label=""
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    required
+                  />
+
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                  >
+                    Log In
+                  </Button>
+                </form>
+
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    backgroundColor: "rgba(0, 0, 0, 0.5)",
+                    zIndex: 1,
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Typography variant="body2" color="textSecondary">
+                    Feature Coming Soon
+                  </Typography>
+                </Box>
               </Box>
-            </Box>
-          )}
-        </Box>
-      </Paper>
-    </Box>
+            )}
+          </Box>
+        </Paper>
+      </Box>
+    </>
   );
 }
