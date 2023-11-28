e667ba794175eb8cbce83c9ea79612317302f760
PaulinaWywrot <wywrot.paula@gmail.com>
2023-11-28 08:02:45 +0000

    correct factor model
    changed Type to type

--- server/models/factor.js
+++ server/models/factor.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Factor extends Model {
     /**
@@ -9,19 +7,22 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({FactorExpectation}) {
+    static associate({ FactorExpectation }) {
       // define association here
-      this.hasMany(FactorExpectation, { foreignKey: 'factorId' });
+      this.hasMany(FactorExpectation, { foreignKey: "factorId" });
     }
   }
-  Factor.init({
-    name: {
-      Type: DataTypes.STRING
+  Factor.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      sequelize,
+      tableName: "",
+      modelName: "Factor",
     }
-  }, {
-    sequelize,
-    tableName: "",
-    modelName: 'Factor',
-  });
+  );
   return Factor;
 };
