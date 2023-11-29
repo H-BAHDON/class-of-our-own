716be39a28c017a22ff80b347dc3256d8ba505b6
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-11-29 02:13:38 +0000

    Created TraineeDashboard

---
+++ client/src/pages/Trainee_Dashboard/TraineeDashboard.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import Sidebar from "./componets/SideBar";
+import MainContent from "./componets/MainContent";
+import { Container, Box } from "@mui/material";
+
+const TraineeDashboard = () => {
+  const [selectedTab, setSelectedTab] = useState('overview');
+
+  const handleTabClick = (tab) => {
+    setSelectedTab(tab);
+  };
+
+  return (
+          <>
+            <Sidebar selectedTab={selectedTab} onTabClick={handleTabClick} />
+        
+            <MainContent selectedTab={selectedTab} />
+          </> 
+  );
+};
+
+export default TraineeDashboard;
