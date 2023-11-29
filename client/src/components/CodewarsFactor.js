dd9c1b03859d83439bb5081f1195550f4bcd4692
seyyednavid hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-11-29 18:33:33 +0000

    Add useAuth for getting the current user

--- client/src/components/CodewarsFactor.js
+++ client/src/components/CodewarsFactor.js
@@ -1,30 +1,28 @@
 import React, { useState, useEffect } from "react";
 import axios from "../config/configAxios";
+import { useAuth } from "../hooks/useAuth";
 
 const CodewarsFactor = () => {
-    const [codewarsFactor, setCodewarsFactor] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
+  const [codewarsFactor, setCodewarsFactor] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const { user, login, loading } = useAuth();
 
-    useEffect(() => {
-        setIsLoading(true);
-        const instant = axios.configAxios();
-        console.log(instant());
-        instant
-          .get("/getRankAndFactorExpectation/:{}")
-          .then((data) => {
-            console.log(data);
-            setCodewarsFactor(data);
-            console.log(codewarsFactor);
-            setIsLoading(false);
-          })
-          .catch((error) => {
-              console.log(error);
-              setIsLoading(false);
-          });
-      }, []);
-  return (
-    <>{codewarsFactor}</>
-  ) ;
+  useEffect(() => {
+    setIsLoading(true);
+    const instant = axios.configAxios();
+    instant
+      .get(`/getRankAndFactorExpectation/:{user.userInfo.email}`)
+      .then((data) => {
+        console.log(data);
+        setCodewarsFactor(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
+  }, []);
+  return <>{codewarsFactor}</>;
 };
 
 export default CodewarsFactor;
