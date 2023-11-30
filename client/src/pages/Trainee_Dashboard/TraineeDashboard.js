094ae80311b7b7730f3c9023ed8ff3ac976252a7
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-11-30 04:34:08 +0000

    Updated TraineeDashboard

--- client/src/pages/Trainee_Dashboard/TraineeDashboard.js
+++ client/src/pages/Trainee_Dashboard/TraineeDashboard.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import Sidebar from "./componets/SideBar";
 import MainContent from "./componets/MainContent";
 import { Container, Box } from "@mui/material";
-import CurrentMilestone from "../../components/CurrentMilestone"
-import CodewarsFactor from '../../components/CodewarsFactor';
 
 const TraineeDashboard = () => {
   const [selectedTab, setSelectedTab] = useState('overview');
@@ -15,7 +13,7 @@ const TraineeDashboard = () => {
   return (
           <>
             <Sidebar selectedTab={selectedTab} onTabClick={handleTabClick} />
-            <CodewarsFactor />
+         
         
             <MainContent selectedTab={selectedTab} />
           </> 
