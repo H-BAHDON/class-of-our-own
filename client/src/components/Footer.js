ad02ea604423628b3d27ab5cca59a771b61a8bc0
PaulinaWywrot <wywrot.paula@gmail.com>
2023-12-04 11:41:51 +0000

    Update Footer.js
    -changed size and color of the font
    - changed height of the footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer style={{ position: 'fixed', bottom: 0, left: 0, right: 0, padding: 0, margin: 0, backgroundColor: '#f8f2ed', color: '#1c1e21', textAlign: 'center', height: '3rem', lineHeight: '2.5rem', fontWeight:"bold" }}>
-      © 2023 <span style={{color:"#ED4343"}}>Code</span>YourFuture 'A Class of Our Own'
+    <footer
+      style={{
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        padding: 0,
+        margin: 0,
+        backgroundColor: "#f8f2ed",
+        color: "#6c6f75",
+        textAlign: "center",
+        height: "2.5rem",
+        lineHeight: "2.5rem",
+        fontWeight: "bold",
+        fontSize: "0.8rem",
+      }}
+    >
+      © 2023 <span style={{ color: "#ED4343" }}>Code</span>YourFuture 'A Class
+      of Our Own'
     </footer>
   );
-}
+};
 
 export default Footer;
