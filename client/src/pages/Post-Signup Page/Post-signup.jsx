2a97f6ae4e0d3b50b85922c4f48b8272cab5ff22
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-12-04 17:10:18 +0000

    Updated Post-signup.jsx

--- client/src/pages/Post-Signup Page/Post-signup.jsx
+++ client/src/pages/Post-Signup Page/Post-signup.jsx
@@ -43,7 +43,7 @@ export default function PostSignup() {
       }
       const responseData = await response.json();
       console.log(responseData);
-      window.location.href = "http://localhost:3000/trainee";
+      window.location.href = "http://localhost:3000";
     } catch (error) {
       console.error("Error sending data to the backend:", error);
     }
