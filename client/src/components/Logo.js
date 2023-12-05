2d90c80835133abddef2f31450bc663307ef72d2
PaulinaWywrot <wywrot.paula@gmail.com>
2023-12-05 14:29:46 +0000

    Changed logo

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -1,8 +1,8 @@
 import React from "react";
-import cyf_brand from "../cyf_brand.png";
+import cyf_logo from "../cyf_logo.PNG";
 
 const Logo = () => {
-  return <img src={cyf_brand} alt="cyf_brand" style={{ height: "100%" }} />;
+  return <img src={cyf_logo} alt="cyf_brand" style={{ height: "100%" }} />;
 };
 
 export default Logo;
